f241b54b8a655d2f69cb4b5b90de4a80cbe8739e
iiAdham <iiadham010@gmail.com>
2024-10-29 21:03:52 +0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+$('.owl-carousel').owlCarousel({
+    rtl: true,
+    margin:10,
+    navElement:"test",
+    nav:true,
+    dots: false,
+    autoplay: true,
+    responsive:{
+        0:{
+            items:5
+        }
+    }
+})
