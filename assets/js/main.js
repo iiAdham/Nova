9da23f2a7366b2cbf0aec5951fb1f58da308f106
iiAdham <iiadham010@gmail.com>
2024-10-31 04:25:43 +0300

    finalllll

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,45 +14,48 @@ $('.owl-carousel').owlCarousel({
 
 // Drag functionallity for products slider
 
-const carousel = document.getElementById('carouselProducts');
-let isDragging = false;
-let startX;
-let currentTranslate = 0;
+const carousels = document.querySelectorAll('.carouselProducts');
 
-carousel.addEventListener('mousedown', (e) => {
-    isDragging = true;
-    startX = e.pageX;
-    currentTranslate = 0;
-    carousel.classList.add('grabbing');
-    e.preventDefault();
-});
+carousels.forEach((carousel) => {
+    let isDragging = false;
+    let startX;
+    let currentTranslate = 0;
 
-carousel.addEventListener('mousemove', (e) => {
-    if (!isDragging) return;
-    const x = e.pageX;
-    currentTranslate = x - startX;
-});
+    carousel.addEventListener('mousedown', (e) => {
+        isDragging = true;
+        startX = e.pageX;
+        currentTranslate = 0;
+        carousel.classList.add('grabbing');
+        e.preventDefault();
+    });
 
-carousel.addEventListener('mouseup', () => {
-    isDragging = false;
-    carousel.classList.remove('grabbing'); 
+    carousel.addEventListener('mousemove', (e) => {
+        if (!isDragging) return;
+        const x = e.pageX;
+        currentTranslate = x - startX;
+    });
 
-    if (currentTranslate < -100) {
-        new bootstrap.Carousel(carousel).prev(); 
-    } else if (currentTranslate > 100) {
-        new bootstrap.Carousel(carousel).next(); 
-    }
-    currentTranslate = 0;
-});
-
-carousel.querySelectorAll('img').forEach(img => {
-    img.addEventListener('dragstart', (e) => e.preventDefault());
-});
-
-carousel.addEventListener('mouseleave', () => {
-    if (isDragging) {
+    carousel.addEventListener('mouseup', () => {
         isDragging = false;
         carousel.classList.remove('grabbing');
+
+        if (currentTranslate < -100) {
+            new bootstrap.Carousel(carousel).prev();
+        } else if (currentTranslate > 100) {
+            new bootstrap.Carousel(carousel).next();
+        }
         currentTranslate = 0;
-    }
+    });
+
+    carousel.querySelectorAll('img').forEach(img => {
+        img.addEventListener('dragstart', (e) => e.preventDefault());
+    });
+
+    carousel.addEventListener('mouseleave', () => {
+        if (isDragging) {
+            isDragging = false;
+            carousel.classList.remove('grabbing');
+            currentTranslate = 0;
+        }
+    });
 });
